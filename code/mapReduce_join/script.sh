8d0928b54037214662f0201117ef946e15339821
Andrea <andrea.alberti01@universitadipavia.it>
2023-09-03 19:22:41 +0200

    new script

--- code/mapReduce_join/script.sh
+++ code/mapReduce_join/script.sh
@@ -1,17 +1,21 @@
-# Run a Hadoop Streaming job using the Hadoop Streaming JAR file
+# Specify the Hadoop Streaming JAR file
+HADOOP_STREAMING_JAR="$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar"
 
-# Specify the files to be distributed to Hadoop nodes (Mapper and Reducer scripts)
-hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
--files join_mapper.py,join_reducer.py \
-
-# Specify the Mapper script to be used for the job (join_mapper.py)
--mapper join_mapper.py \
-
-# Specify the Reducer script to be used for the job (join_reducer.py)
--reducer join_reducer.py \
+# Specify the Mapper and Reducer scripts
+MAPPER_SCRIPT="/code/join_mapper.py"
+REDUCER_SCRIPT="/code/join_reducer.py"
 
 # Specify the input data sources (CSV files to be processed)
--input /path/to/books_ratings.csv,/path/to/books_info.csv \
+INPUT_FILES="hdfs://localhost:9900/user/book_reviews/books_data.csv,hdfs://localhost:9900/user/book_reviews/books_rating.csv"
 
 # Specify the output directory where the job results will be stored
--output /path/to/output_directory
+OUTPUT_DIR="hdfs://localhost:9900/user/book_reviews/output"
+
+# Run the Hadoop Streaming job
+hadoop jar $HADOOP_STREAMING_JAR \
+-files $MAPPER_SCRIPT,$REDUCER_SCRIPT \
+-mapper "python $MAPPER_SCRIPT" \
+-reducer "python $REDUCER_SCRIPT" \
+-input $INPUT_FILES \
+-output $OUTPUT_DIR
+
