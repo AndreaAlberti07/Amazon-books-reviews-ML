69a858400a558f36c5906a30d14e4ba1abdfb9ef
Andrea <andrea.alberti01@universitadipavia.it>
2023-09-03 11:11:32 +0200

    Added Map-Reduce alternative options

--- code/mapReduce_join/join_mapper.py
+++ code/mapReduce_join/join_mapper.py
@@ -1,3 +1,7 @@
+# ---------------------------------------------------------------------------------------------
+# ---------------------------------[ OPTION 1 ]------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+
 import sys
 
 # This function reads the 'Books Ratings' table and emits ('title', ('rating', 'other_data')) for each record.
@@ -5,12 +9,13 @@ for line in sys.stdin:
     line = line.strip()
     columns = line.split(',')
 
-    # Check if there are 8 columns (Assuming 8 columns in the 'Books Ratings' CSV)
-    if len(columns) == 8:
-        book_title = columns[1].strip()  # Extract the book title
-        rating = columns[6].strip()      # Extract the rating
+    '''Valid if the Id columns is deleted from the 'Books Ratings' table'''
+    # Check if there are 9 columns (Assuming 9 columns in the 'Books Ratings' CSV)
+    if len(columns) == 9:
+        book_title = columns[0].strip()  # Extract the book title
+        rating = columns[5].strip()      # Extract the rating
         # Join other data columns
-        other_data = '\t'.join(columns[0:6] + columns[7:])
+        other_data = '\t'.join(columns[0:5] + columns[6:])
         # Emit the data in the format 'title    rating    R    other_data'
         print(f"{book_title}\t{rating}\tR\t{other_data}")
 
@@ -24,4 +29,29 @@ for line in sys.stdin:
         book_title = columns[0].strip()  # Extract the book title
         info_data = '\t'.join(columns[1:])  # Join all other info data columns
         # Emit the data in the format 'title    info_data    I    -' where '-' represents no additional data
-        print(f"{book_title}\t{info_data}\tI\t-")
+        print(f"{book_title}{info_data}\tI\t-")
+        
+# ---------------------------------------------------------------------------------------------
+# ---------------------------------[ OPTION 2 ]------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+
+'''Alternative option'''
+
+# Path: code/mapReduce_join/join_mapper.py
+
+import sys
+
+for line in sys.stdin:
+    columns = line.strip().split(',')
+    
+    # Check which table is being read
+    if len(columns)==9: # 'Books Ratings' table
+        title = columns[1] # Extract the book title
+        other_data = '\t'.join(columns[0]+columns[2:]) # Join all other data columns
+        print(f"{title}\t{other_data}\tR")
+    
+    else: # 'Books Data' table
+        title = columns[0] # Extract the book title
+        other_data = '\t'.join(columns[1:]) # Join all other data columns
+        print(f"{title}\t{other_data}\tD")
+    
--- code/mapReduce_join/join_reducer.py
+++ code/mapReduce_join/join_reducer.py
@@ -1,3 +1,7 @@
+# ---------------------------------------------------------------------------------------------
+# ---------------------------------[ OPTION 1 ]------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+
 import sys
 
 # Initialize variables to keep track of the current book title and data lists
@@ -41,3 +45,60 @@ if current_title:
     for r_data in ratings_data:
         for i_data in info_data:
             print(f"{current_title}\t{r_data}\t{i_data}")
+            
+# ---------------------------------------------------------------------------------------------
+# ---------------------------------[ OPTION 2 ]------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+            
+'''Alternative option'''
+
+# Path: code/mapReduce_join/join_reducer.py
+
+import sys
+
+data_table = []
+rating_table = []
+
+for line in sys.stdin:
+    columns = line.strip().split('\t')
+    
+    # Check the source of the data and prepare two separate lists
+    source = columns[-1]
+    if source == 'D':
+        data_table.append(columns[:-1])
+    else:
+        rating_table.append(columns[:-1])
+    
+# Iterate through the two lists and join the data
+for data in data_table:
+    for rating in rating_table:
+        if data[0] == rating[0]: # Check if the book titles match
+            other_data = '\t'.join(data[1:] + rating[1:])
+            print(f"{data[0]}{other_data}")
+            
+# ---------------------------------------------------------------------------------------------
+# ---------------------------------[ OPTION 3 ]------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+        
+'''Even more efficient: exploit the sorting mechanism embedded in the Hadoop Map-Reduce framework'''
+
+import sys
+
+data_table = {}  # Create a dictionary for 'Books Data' table
+
+for line in sys.stdin:
+    columns = line.strip().split('\t')
+    source = columns[-1]
+    key = columns[0]  # Assuming the book title is the join key
+
+    if source == 'D':
+        data_table[key] = columns[1:-1]  # Store data in the dictionary
+    else:
+        rating_data = columns[1:-1]  # Remove the table identifier
+
+        # Check if the book title is in the 'Books Data' dictionary
+        if key in data_table:
+            data = data_table[key]
+            # Join 'Books Data' and 'Books Ratings' data
+            joined_data = '\t'.join(data + rating_data)
+            print(f"{key}{joined_data}")
