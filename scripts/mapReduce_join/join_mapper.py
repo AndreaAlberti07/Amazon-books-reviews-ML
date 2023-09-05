404b6acb62e9340e722f56bd8db03b738173b775
DavideLigari01 <davide.ligari01@universitadipavia.it>
2023-09-05 14:52:51 +0200

    fixing mapReduce #5

--- scripts/mapReduce_join/join_mapper.py
+++ scripts/mapReduce_join/join_mapper.py
@@ -4,14 +4,6 @@ import sys
 
 for line in sys.stdin:
     columns = line.strip().split(',')
-    # Check which table is being read
-    if len(columns) == 10:  # 'Books Ratings' table
-        title = columns[0]  # Extract the book title
-        other_data = ','.join(columns[1:])  # Join all other data columns
-        print(f"{title}\t{other_data}\tD\n")
-
-    else:  # 'Books Rating' table
-        title = columns[0]  # Extract the book title
-        # Join all other data columns
-        other_data = ','.join(columns[1:])
-        print(f"{title}\t{other_data}\tR\n")
+    title = columns[0]  # Extract the book title
+    other_data = ','.join(columns[1:])  # Join all other data columns
+    print(f"{title}\t{other_data}")
--- scripts/mapReduce_join/join_reducer.py
+++ scripts/mapReduce_join/join_reducer.py
@@ -9,22 +9,30 @@ print('Title,description,authors,image,previewLink,publisher,publishedDate,infoL
 for line in sys.stdin:
     columns = line.strip().split('\t')
     source = columns[-1]
+    values = source.split(',')
+    table = values[-1]
+    values = values[:-1]
     key = columns[0]  # Assuming the book title is the join key
     if key == 'Title':
         continue
 
-    if source == 'D':
-        book_data = columns[1:-1]
+    if table == 'D':
+        book_data = values
         data_table[key] = book_data  # Store data in the dictionary
 
+        # Check if the book title is in the 'Books Rating' dictionary
         if key in rating_table:
-            rating_data = rating_table[key]
-            # Join 'Books Data' and 'Books Ratings' data
-            joined_data = ','.join(book_data + rating_data)
-            print(f"{key},{joined_data}")
+            rows = rating_table[key]
+            for rating_data in rows:
+                # Join 'Books Data' and 'Books Ratings' data
+                joined_data = ','.join(data + rating_data)
+                print(f"{key},{joined_data}")
     else:
-        rating_data = columns[1:-1]  # Remove the table identifier
-        rating_table[key] = rating_data  # Store data in the dictionary
+        rating_data = values  # Remove the table identifier
+        if key in rating_table:
+            rating_table[key].append(rating_data)
+        else:
+            rating_table[key] = [rating_data]
         # Check if the book title is in the 'Books Data' dictionary
         if key in data_table:
             data = data_table[key]
