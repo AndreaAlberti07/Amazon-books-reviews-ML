9a16745d784cc650a84ab6a031b7d28b2a89450e
Andrea <andrea.alberti01@universitadipavia.it>
2023-09-08 01:09:50 +0200

    MapReduce modified #5

--- scripts/mapReduce_join/join_reducer.py
+++ scripts/mapReduce_join/join_reducer.py
@@ -10,7 +10,7 @@ for line in sys.stdin:
 	# remove leading and trailing whitespace
 	line = line.strip()
 	# parse the input from mapper.py
-	fields = [el.strip() for el in line.split('\t')]
+	fields = line.split('\t')
  
 	if fields[1] == '-':
 		# Save the title and genre of a movie
@@ -18,7 +18,6 @@ for line in sys.stdin:
 		values = fields[9:]
 	else:
 		if fields[0] == title:
-            values = values[2:]
-            # Title, description, authors, publisher, publishedDate, categories, Price, User_id, profileName, review/helpfulness, review/score, review/time, review/summary, review/text
-            output = title + '\t' + '\t'.join(values) + '\t'+'\t'.join(fields[1:9])
+			# Title, description, authors, publisher, publishedDate, categories, Price, User_id, profileName, review/helpfulness, review/score, review/time, review/summary, review/text
+			output = title + '\t' + '\t'.join(values) + '\t'+'\t'.join(fields[1:9])
 			print(output)
