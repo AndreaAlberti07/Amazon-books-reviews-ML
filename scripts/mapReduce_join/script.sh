d3e2a29f6458f3e4219cf8c0f090f8f4e9413008
Andrea <andrea.alberti01@universitadipavia.it>
2023-09-08 11:39:13 +0200

    Updated MapReduce (no punctuations removed) #5

--- scripts/mapReduce_join/script.sh
+++ scripts/mapReduce_join/script.sh
@@ -6,7 +6,7 @@ MAPPER_SCRIPT="join_mapper.py"
 REDUCER_SCRIPT="join_reducer.py"
 
 # Specify the input data sources (CSV files to be processed)
-INPUT_FILES="hdfs://localhost:9900/user/book_reviews/books_data_cleaned.csv,hdfs://localhost:9900/user/book_reviews/books_rating_cleaned.csv"
+INPUT_FILES="hdfs://localhost:9900/user/book_reviews/books_data_cleaned_v2.csv,hdfs://localhost:9900/user/book_reviews/books_rating_cleaned_v2.csv"
 
 # Specify the output directory where the job results will be stored
 OUTPUT_DIR="hdfs://localhost:9900/user/book_reviews/output"
