98d099e8b12bf08f2cafae9dc39768faa9c29446
DavideLigari01 <davide.ligari01@universitadipavia.it>
2023-09-17 17:41:52 +0200

    training random forest (WIP #7) and writing report

--- mapReduce_join/join_reducer.py
+++ mapReduce_join/join_reducer.py
@@ -3,30 +3,33 @@
 
 import sys
 
+# Initialize variables to keep track of the current key, data values, and rating values.
 current_key = None
 data_values = []
 rating_values = []
 
 for line in sys.stdin:
-    # Remove leading and trailing whitespace
     line = line.strip()
-    # Parse the input from mapper.py
-    fields = line.split('\t')
 
+    fields = line.split('\t')
     key = fields[0]
+
+    # Check if the record belongs to the data table (fields[1] == '-').
     if fields[1] == '-':
-        # Data table record
         data_values = fields[2:]
     else:
         # Rating table record
         rating_values = fields[2:]
 
-    # Check if the key has changed (new record)
+    # Check if the key has changed (new record).
     if current_key != key:
         current_key = key
     else:
-        # Emit the joined record when both data and rating values are available
+        # Emit the joined record when both data and rating values are available.
         if data_values and rating_values:
+            # Combine the current key, data values, and rating values with tab separators.
             output = current_key + '\t' + \
                 '\t'.join(data_values) + '\t' + '\t'.join(rating_values)
+
+            # Print the joined record as the output.
             print(output)
